0fb0590157fece1a0e120873811aabb40b145c1a
Sebastian <alu0101131108@ull.edu.es>
2021-04-11 16:58:38 +0100

    ConstructiveGreedy seems to work fine

--- src/ConstructiveGreedy.cpp
+++ src/ConstructiveGreedy.cpp
@@ -4,39 +4,66 @@ void ConstructiveGreedy::run(const std::vector<Task> &tasks, std::vector<Machine
 {
   int nTasks = tasks.size();
   int nMachines = machines.size();
+  int totalCt = 0;
   std::vector<std::vector<Task>> schedules(nMachines, std::vector<Task>());
-  std::vector<int> machineTCT(nMachines, 0);
   std::vector<int> scheduledIndexes;
 
   // Add one initial task for every machine.
-  int minCompletionTime, minCtIndex;
+  int minCtInc, minCtTask;
   for (int m = 0; m < nMachines; m++)
   {
-    minCompletionTime = UPPER_TIME_LIMIT;
-    minCtIndex = -1;
+    minCtInc = UPPER_TIME_LIMIT;
     for (int j = 1; j < nTasks; j++)
     {
       // Check only tasks that are not scheduled yet.
       if (std::find(scheduledIndexes.begin(), scheduledIndexes.end(), j) == scheduledIndexes.end() &&
-          (setupTimes.get(0, j) + tasks[j - 1].getProcessTime()) < minCompletionTime)
+          (setupTimes.get(0, j) + tasks[j - 1].getProcessTime()) < minCtInc)
       {
-        minCompletionTime = setupTimes.get(0, j) + tasks[j - 1].getProcessTime();
-        minCtIndex = j;
+        minCtInc = setupTimes.get(0, j) + tasks[j - 1].getProcessTime();
+        minCtTask = j;
       }
     }
-    schedules[m].push_back(tasks[minCtIndex - 1]);
-    machineTCT[m] = setupTimes.get(0, minCtIndex) + tasks[minCtIndex - 1].getProcessTime();
-    scheduledIndexes.push_back(minCtIndex);
+    schedules[m].push_back(tasks[minCtTask - 1]);
+    totalCt += setupTimes.get(0, minCtTask) + tasks[minCtTask - 1].getProcessTime();
+    scheduledIndexes.push_back(minCtTask);
   }
 
-  // Add tasks that minimize TCT to each machine.
-  
+  // Search, for each machine, the task that does fewer increment to its completion time when added to its schedule.
+  int minTCtTask, minTCtInc, minCtMachineIndex; // For the outter search. Best machine to insert its own best task to insert.
+  int lastTask;                                 // For the inner search. Best task to insert for each machine. (Also minCtInc, minCtTask)
+  while (scheduledIndexes.size() < nTasks)
+  {
+    minTCtInc = UPPER_TIME_LIMIT;
+    for (int m = 0; m < nMachines; m++)
+    {
+      lastTask = schedules[m][schedules[m].size() - 1].getId();
+      minCtInc = UPPER_TIME_LIMIT;
+      for (int j = 1; j < nTasks; j++)
+      {
+        // Check only tasks that are not scheduled yet.
+        if (std::find(scheduledIndexes.begin(), scheduledIndexes.end(), j) == scheduledIndexes.end() &&
+            (setupTimes.get(lastTask, j) + tasks[j - 1].getProcessTime()) < minCtInc)
+        {
+          minCtInc = setupTimes.get(lastTask, j) + tasks[j - 1].getProcessTime();
+          minCtTask = j;
+        }
+      }
+      if (minCtInc < minTCtInc)
+      {
+        minTCtInc = minCtInc;
+        minCtMachineIndex = m;
+        minTCtTask = minCtTask;
+      }
+    }
+    schedules[minCtMachineIndex].push_back(tasks[minTCtTask - 1]);
+    totalCt += minTCtInc;
+    scheduledIndexes.push_back(minTCtTask);
+  }
 
   // Set schedules to corresponding machines.
   for (int m = 0; m < nMachines; m++)
   {
     machines[m].setSchedule(schedules[m]);
-    std::cout << "TCT: " << machineTCT[m] << std::endl;
   }
 }
 
